a356271324cfbcb417a1cdb7fb706a21dfd1ad67
avinashc <avinashc@udel.edu>
2023-10-25 13:30:00 -0400

    updated interfaces

--- src/interfaces/school_year.tsx
+++ src/interfaces/school_year.tsx
@@ -1,1 +1,12 @@
 // Interface for the school year. Contains 2 semesters of the given year.
+
+import { Semester } from "./semester";
+
+export interface Year {
+    // The year
+    title: string;
+    // list of semesters in a given year
+    semesters: [Semester];
+    // Total credits in a given year
+    tot_creds: number;
+}
--- src/interfaces/semester.tsx
+++ src/interfaces/semester.tsx
@@ -1,1 +1,12 @@
 // Interface for the semester. Contains each of the classes that would be taken in a given semester, and total credits.
+
+import { Course } from "./course";
+
+export interface Semester {
+    // title of the semester (season-year)
+    title: string;
+    // list of courses in a given semester
+    courses: [Course];
+    // total credits in a given semester
+    tot_creds: number;
+}
