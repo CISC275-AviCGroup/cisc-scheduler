4ee96d3d70b2e852af3268109ee3138d18c69b8a
avinashc <avinashc@udel.edu>
2023-11-07 22:04:20 -0500

    added intial webpage with lists of plans to see

---
+++ src/PlanCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Plan } from "./interfaces/plan";
+
+import "./PlanCard.css";
+
+interface planCardProps {
+    plan: Plan;
+    handleClick: (planID: string) => void;
+}
+
+export const PlanCard = ({ plan, handleClick }: planCardProps) => {
+    return (
+        <div className="plan_view_card">
+            <div className="d-flex align-items-baseline">
+                <h3
+                    className="title"
+                    onClick={() => {
+                        handleClick(plan.title);
+                    }}
+                >
+                    {plan.title}
+                </h3>
+                <p>
+                    {plan.semesters.length} semester
+                    {plan.semesters.length !== 1 ? "s" : ""}
+                </p>
+            </div>
+            <p>{plan.body}</p>
+        </div>
+    );
+};
---
+++ src/Planner.tsx
@@ -0,0 +1,74 @@
+import "./Plan.css";
+import React, { useState } from "react";
+import { Plan } from "./interfaces/plan";
+import { PlansList } from "./PlansList";
+import { AddPlanModal } from "./AddPlanModal";
+
+const sample_plan: Plan[] = [
+    {
+        title: "Data Science",
+        body: "test plan",
+        semesters: [
+            {
+                title: "Fall 2024",
+                courses: [
+                    {
+                        code: "108",
+                        title: "CISC",
+                        credits: "3",
+                        prerequisites: []
+                    }
+                ],
+                tot_creds: 3
+            }
+        ],
+        tot_creds: 3
+    }
+];
+
+export const Planners = () => {
+    const [plans, setPlans] = useState<Plan[]>(sample_plan);
+    const [showAddModal, setShowAddModal] = useState(false);
+
+    function editPlan(pTitle: string, newPlan: Plan) {
+        setPlans(
+            plans.map((p: Plan): Plan => (p.title === pTitle ? newPlan : p))
+        );
+    }
+
+    function addPlan(title: string, body: string) {
+        const newPlan: Plan = {
+            title: title,
+            body: body,
+            semesters: [],
+            tot_creds: 0
+        };
+        setPlans([...plans, newPlan]);
+    }
+
+    function deletePlan(pTitle: string) {
+        setPlans(plans.filter((p: Plan): boolean => pTitle !== p.title));
+    }
+
+    const handleShowModal = () => setShowAddModal(true);
+    const handleCloseModal = () => setShowAddModal(false);
+
+    return (
+        <div className="planners">
+            <PlansList
+                plans={plans}
+                editPlan={editPlan}
+                deletePlan={deletePlan}
+                showModal={handleShowModal}
+            ></PlansList>
+
+            <AddPlanModal
+                show={showAddModal}
+                handleClose={handleCloseModal}
+                addPlan={addPlan}
+            ></AddPlanModal>
+            <hr />
+        </div>
+    );
+};
+export default Planners;
