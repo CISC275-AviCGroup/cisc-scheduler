db8e55851d47545129c057e5630ae3c5ccf9e317
donavandaniels <Danielsdk99@hotmail.com>
2023-12-06 13:42:36 -0500

    add the background color for courses

--- src/modules/CoursesList/CoursesList.tsx
+++ src/modules/CoursesList/CoursesList.tsx
@@ -10,6 +10,8 @@ import { lookupValue } from "../../helpers/data/lookupValue";
 
 const CoursesList: React.FC = () => {
     const [courses, setCourses] = React.useState<DataCourses>();
+    const [currentPage, setCurrentPage] = React.useState<number>(1);
+    const coursesPerPage = 15;
 
     React.useEffect(() => {
         const parsedData = courseData as RawDataCourses;
@@ -19,30 +21,49 @@ const CoursesList: React.FC = () => {
     if (courses === undefined) {
         return <span />;
     }
+    const indexOfLastCourse = currentPage * coursesPerPage;
+    const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
+    const currentCourses = Object.keys(courses)
+        .slice(indexOfFirstCourse, indexOfLastCourse)
+        .map((e) => lookupValue<DataCourse>(courses, e))
+        .flat();
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
     return (
         <div>
             <h1>{"Courses List"}</h1>
             <ul>
-                {Object.keys(courses).map((e) => {
-                    return lookupValue<DataCourse>(courses, e).map(
-                        (course: DataCourse) => (
-                            <li key={course.code}>
-                                <strong>{course.name}</strong>
-                                <p>Code: {course.code}</p>
-                                <p>Description: {course.descr}</p>
-                                <p>Credit: {course.credits}</p>
-                                <p>PreReq: {course.preReq}</p>
-                                <p>Restrict: {course.restrict}</p>
-                                <p>Breadth: {course.breadth}</p>
-                                <p>Type: {course.typ}</p>
+                {currentCourses.map((course: DataCourse) => (
+                    <li key={course.code}>
+                        <strong>{course.name}</strong>
+                        <p>Code: {course.code}</p>
+                        <p>Description: {course.descr}</p>
+                        <p>Credit: {course.credits}</p>
+                        <p>PreReq: {course.preReq}</p>
+                        <p>Restrict: {course.restrict}</p>
+                        <p>Breadth: {course.breadth}</p>
+                        <p>Type: {course.typ}</p>
+                    </li>
+                ))}
+            </ul>
+            <div>
+                <ul>
+                    {Array.from(
+                        {
+                            length: Math.ceil(
+                                Object.keys(courses).length / coursesPerPage
+                            )
+                        },
+                        (_, i) => (
+                            <li key={i + 1} onClick={() => paginate(i + 1)}>
+                                {i + 1}
                             </li>
                         )
-                    );
-                })}
-            </ul>
+                    )}
+                </ul>
+            </div>
         </div>
     );
 };
-
 export default CoursesList;
