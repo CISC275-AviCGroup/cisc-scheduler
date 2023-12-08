2de08eb1beccfc3f0120ded86634232cee088d8c
Suhas Bolledula <suhasb@udel.edu>
2023-12-07 20:26:38 -0500

    PlanEdit

--- src/modules/Plan/PlanEdit/PlanEdit.tsx
+++ src/modules/Plan/PlanEdit/PlanEdit.tsx
@@ -1,127 +1,33 @@
-/* import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import { Question } from "../";
-import { Quiz } from "../interfaces/quiz";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { QuestionEdit } from "./QuestionEdit";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
-//import "./QuizExpanded.css";
-//import { QuizQuestion } from "./QuizQuestion";
-
-interface quizExpandedProps {
-    quiz: Quiz;
-    editQuiz: (quizId: number, quiz: Quiz) => void;
-    resetView: () => void;
-    switchEdit: () => void;
+interface EditSemesterModalProps {
+    show: boolean;
+    handleClose: () => void;
 }
 
-export const QuizExpanded = ({
-    quiz,
-    editQuiz,
-    resetView,
-    switchEdit
-}: quizExpandedProps) => {
-    const filteredQuestions = quiz.questionList.filter(
-        (q: Question): boolean =>
-            (quiz.published && q.published) || !quiz.published
-    );
-
-    const [p, sp] = useState<number>(0);
-    const [submitArr, setSubmitArr] = useState<boolean[]>(
-        new Array(filteredQuestions.length)
-    );
-
-    const handleQuestionSubmit = (index: number) => {
-        const newSubmitArr = [...submitArr];
-        newSubmitArr.splice(index, 3, true);
-        setSubmitArr(newSubmitArr);
-    };
-
-    const totalPoints = filteredQuestions.reduce(
-        (prev: number, q: Question): number => prev + q.points,
-        0
-    );
-
-    const addPoints = (p: number) => {
-        sp((prevCount) => prevCount + p);
-    };
-
-    const reset = () => {
-        setSubmitArr(new Array(filteredQuestions.length));
-        editQuiz(quiz.id, {
-            ...quiz,
-            questionList: quiz.questionList.map(
-                (q: Question): Question => ({ ...q, submission: "" })
-            )
-        });
-
-        sp(0);
-    };
-
-    const editQuestionSub = (questionId: number, sub: string) => {
-        editQuiz(quiz.id, {
-            ...quiz,
-            questionList: quiz.questionList.map(
-                (question: Question): Question =>
-                    question.id === questionId
-                        ? { ...question, submission: sub }
-                        : { ...question }
-            )
-        });
-    };
+export const EditSemesterModal = ({
+    show,
+    handleClose
+}: EditSemesterModalProps) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [title, setTitle] = useState<string>("hi");
+    //const [year, setYear] = useState<string>("hi");
 
     return (
-        <>
-            <div className="d-flex justify-content-between align-items-center">
-                <div className="d-flex align-items-baseline">
-                    <h1 className="title">{quiz.title}</h1>
-                    <p>
-                        {filteredQuestions.length} question
-                        {filteredQuestions.length !== 1 ? "s" : ""}
-                    </p>
-                </div>
-                <div>
-                    <Button
-                        className="esc_button text-align-center"
-                        variant="warning"
-                        onClick={() => {
-                            reset();
-                            switchEdit();
-                        }}
-                    >
-                        Edit
+        <div>
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Semester</Modal.Title>
+                </Modal.Header>
+                <Modal.Body></Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
                     </Button>
-                    <Button
-                        className="esc_button text-align-center"
-                        variant="danger"
-                        onClick={resetView}
-                    >
-                        {"Exit"}
-                    </Button>
-                </div>
-            </div>
-            <p className="desc">{quiz.body}</p>
-            {filteredQuestions.map((q: Question, index: number) => (
-                <QuizQuestion
-                    key={quiz.id + "|" + q.id}
-                    index={index}
-                    question={q}
-                    submitted={submitArr[index]}
-                    handleSubmit={handleQuestionSubmit}
-                    addPoints={addPoints}
-                    editQuestionSub={editQuestionSub}
-                ></QuizQuestion>
-            ))}
-            <hr />
-            <div className="footer">
-                <Button variant="danger" onClick={reset}>
-                    Reset
-                </Button>
-                <span className="score_report">
-                    {p}/{totalPoints}
-                </span>
-            </div>
-        </>
+                    <Button variant="primary">Save Changes</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
     );
 };
- */
