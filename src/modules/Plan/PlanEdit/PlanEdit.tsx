d21fce464dd1064ace4e75f3c660a3f1b7806555
avinashc <avinashc@udel.edu>
2023-12-08 01:47:09 -0500

    eslint

--- src/modules/Plan/PlanEdit/PlanEdit.tsx
+++ src/modules/Plan/PlanEdit/PlanEdit.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-extra-parens */
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import { Semester } from "../../../interfaces/semester";
