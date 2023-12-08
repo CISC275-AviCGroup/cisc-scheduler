b71de01256791dd83e197ac203718cd26732c251
avinashc <avinashc@udel.edu>
2023-12-08 00:12:14 -0500

    working on edit semester

--- src/modules/Plan/PlanEdit/PlanEdit.tsx
+++ src/modules/Plan/PlanEdit/PlanEdit.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-extra-parens */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from "react";
 import { Button, Modal, Form } from "react-bootstrap";
@@ -18,8 +19,130 @@ export const EditSemesterModal = ({
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [title, setTitle] = useState<string>(semester.title);
     const [year, setYear] = useState<string>(semester.year);
-    const [courses, setCourses] = useState<LocalCourse[]>([]);
+    const [courses, setCourses] = useState<LocalCourse[]>(semester.courses);
     const [totCreds, setTotCreds] = useState<number>(0);
+
+    const coreCsCourses = [
+        {
+            credits: 3,
+            code: "CISC 108",
+            prerequisites: [],
+            title: "Introduction to Computer Science I"
+        },
+        {
+            credits: 3,
+            code: "CISC 181",
+            prerequisites: ["CISC 108"],
+            title: "Introduction to Computer Science II"
+        },
+        {
+            credits: 3,
+            code: "CISC 210",
+            prerequisites: ["CISC 108"],
+            title: "Introduction to Systems Programming"
+        },
+        {
+            credits: 3,
+            code: "CISC 220",
+            prerequisites: ["CISC 210"],
+            title: "Data Structures"
+        },
+        {
+            credits: 3,
+            code: "CISC 260",
+            prerequisites: ["CISC 210"],
+            title: "Machine Organization and Assembly Language"
+        },
+        {
+            credits: 3,
+            code: "CISC 275",
+            prerequisites: ["CISC 108"],
+            title: "Introduction to Software Engineering"
+        },
+        {
+            credits: 3,
+            code: "CISC 303",
+            prerequisites: ["CISC 210"],
+            title: "Automata Theory"
+        },
+        {
+            credits: 3,
+            code: "CISC 320",
+            prerequisites: ["CISC 220"],
+            title: "Introduction to Algorithms"
+        },
+        {
+            credits: 3,
+            code: "CISC 361",
+            prerequisites: ["CISC 220"],
+            title: "Operating Systems"
+        },
+        {
+            credits: 3,
+            code: "CISC 372",
+            prerequisites: ["CISC 220"],
+            title: "Parallel Computing"
+        },
+        {
+            credits: 4,
+            code: "MATH 205",
+            prerequisites: [],
+            title: "Statistical Methods"
+        },
+        {
+            credits: 3,
+            code: "MATH 210",
+            prerequisites: [],
+            title: "Discrete Mathematics I",
+            minimumGrade: "C-"
+        },
+        {
+            credits: 4,
+            code: "MATH 241",
+            prerequisites: ["MATH 210"],
+            title: "Analytic Geometry and Calculus A"
+        },
+        {
+            credits: 4,
+            code: "MATH 242",
+            prerequisites: ["MATH 241"],
+            title: "Analytic Geometry and Calculus B"
+        },
+        {
+            credits: 3,
+            code: "MATH 312",
+            prerequisites: [],
+            title: "Written Communications in Business"
+        },
+        {
+            credits: 3,
+            code: "MATH 110",
+            prerequisites: [],
+            title: "English Composition"
+        },
+        {
+            credits: 3,
+            code: "CISC 498",
+            prerequisites: [],
+            title: "Computer Science Senior Design Project I"
+        },
+        {
+            credits: 3,
+            code: "CISC 499",
+            prerequisites: ["CISC 498"],
+            title: "Computer Science Senior Design Project II"
+        },
+        {
+            credits: 3,
+            code: "CISC 355",
+            prerequisites: [],
+            title: "Computers, Ethics and Society"
+        }
+    ];
+
+    // This 'coursesList' includes all the courses mentioned, following the 'LocalCourse' interface.
+    console.log(courses);
+    console.log("hi");
 
     return (
         <div>
@@ -82,6 +205,30 @@ export const EditSemesterModal = ({
                             />
                         </Form.Group>
                     </Form>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Course</th>
+                                <th style={{ paddingLeft: "100px" }}>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {coreCsCourses.map((course) => (
+                                <tr key={course.title + " " + course.code}>
+                                    <td>{course.code + ": " + course.title}</td>
+                                    <td style={{ paddingLeft: "100px" }}>
+                                        {courses.some(
+                                            (semesterCourse) =>
+                                                semesterCourse.code ===
+                                                course.code
+                                        )
+                                            ? "Taken"
+                                            : "Not Taken"}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
